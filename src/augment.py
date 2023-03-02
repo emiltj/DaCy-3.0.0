0367949bdd7f40db966fe3258620e23f4bb5f853
Emil Trenckner Jessen <emil.tj@hotmail.com>
2023-03-02 11:07:54 +0100

    minor changes

--- src/augment.py
+++ src/augment.py
@@ -3,15 +3,13 @@
 # from spacy.scorer import Scorer
 # from spacy.training.augment import create_lower_casing_augmenter, dont_augment
 # from wasabi import msg
-
 # augmenters = augmenty.augmenters()
-
 # for augmenter in sorted(list(augmenters.keys())):
 #     print(augmenter)
 
 # Not yet implemented
 
-# Use:
+# For inspiration, use:
 # - https://github.com/centre-for-humanities-computing/DaCy/blob/main/training/v0.1.1/augment.py
 # - https://kennethenevoldsen.github.io/augmenty/tutorials/introduction.html
 # - https://kennethenevoldsen.github.io/augmenty/augmenters_overview.html
